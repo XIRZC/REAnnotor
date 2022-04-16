9c84984b8aae3f302fe5cf5b66bb4e3940232ecf
mrxir <zicongxie@foxmail.com>
2022-04-16 19:00:58 +0800

    fix some small bugs, and now has stable features, mask function developing, mask prepare function is okay

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 # author: mrxirzzz
 
-import os
+# internal packages imported
 import dearpygui.dearpygui as dpg
 import argparse
 from pathlib import Path
 import json
-from numpy import einsum_path
-import random
 
+# project packages imported
 from utils.episodes import Episodes
 
-# Global Variables
-Width_Win, Height_Win, Factor_Win = 1120, 880, 2
-Width_Linux, Height_Linux, Factor_Linux = 1600, 1000, 3
-WIDTH, HEIGHT, FACTOR = Width_Linux, Height_Linux, Factor_Linux
-default_split = 'val_seen'
-default_scene = 3
-default_episode_idx = 4
-default_frame_idx = 1
+
+# Global Constant and Variables
+DEFAULT_SPLIT = 'val_seen'
+DEFAULT_SCENE = 3
+DEFAULT_EPISODE_IDX = 4
+DEFAULT_FRAME_IDX = 1
+
+WIN_FACTOR = 1   # cause my windows has factor 1.5, so need to shrinke resolution
+WINDOW_WIDTH, WINDOW_HEIGHT, FRAME_FACTOR = int(1600 / WIN_FACTOR), int(1000 / WIN_FACTOR), int(3 / WIN_FACTOR)
+FRAME_WIDTH, FRAME_HEIGHT = 256, 144
+
+ORIGIN_FRAME_OFFSET_X, ORIGIN_FRAME_OFFSET_Y = 0, 70
+SEG_FRAME_OFFSET_X, SEG_FRAME_OFFSET_Y = 0, 520
+STATUS_BAR_OFFSET_X, STATUS_BAR_OFFSET_Y = 5, 965
+
+ORIGIN_FRAME_END_X, ORIGIN_FRAME_END_Y = ORIGIN_FRAME_OFFSET_X + FRAME_WIDTH * FRAME_FACTOR, ORIGIN_FRAME_OFFSET_Y + FRAME_HEIGHT * FRAME_FACTOR
+SEG_FRAME_END_X, SEG_FRAME_END_Y = SEG_FRAME_OFFSET_X + FRAME_WIDTH * FRAME_FACTOR, SEG_FRAME_OFFSET_Y + FRAME_HEIGHT * FRAME_FACTOR
+
 episodes, f = None, None
+mouse_point = []
+selectable_item_exp = ''
 
 
 # Json load and write functions
@@ -157,7 +168,6 @@ def idx_callback(sender, app_data, user_data):
 def exps_callback(sender, app_data, user_data):
     exp, mode = user_data[0], user_data[1]
     callback_val = dpg.get_value(sender)
-    print(exp, mode, callback_val)
     episode_idx = int(dpg.get_value('episode_idx'))
     if mode == 'add':
         episodes[episode_idx-1]['expressions'].append(exp)
@@ -190,7 +200,7 @@ def exps_callback(sender, app_data, user_data):
 
 
 # key bind for episode and frame editing functions
-def vim_key_fe_callback(sender, app_data, user_data):
+def keyboard_event_handler(sender, app_data, user_data):
     # h,l for episode minus and plus, j,k for frame plus and minus, like vim motion bind keys
     episodes = user_data
     if app_data in [ 72, 87, 265 ]: # press h or w or up key
@@ -208,7 +218,7 @@ def toggle_bind_key_callback():
     if dpg.is_key_down(dpg.mvKey_E):
         # print('Press Ctrl E!', dpg.get_item_callback('bind_key_h'))
         for i in ls:
-            dpg.configure_item('bind_key_' + i, callback=vim_key_fe_callback)
+            dpg.configure_item('bind_key_' + i, callback=keyboard_event_handler)
     if dpg.is_key_down(dpg.mvKey_R):
         # print('Press Ctrl R!', dpg.get_item_callback('bind_key_h'))
         for i in ls:
@@ -218,7 +228,7 @@ def toggle_bind_key():
     ls = ['h', 'j', 'k', 'l', 'w', 'a', 's', 'd', 'up', 'right', 'left', 'down']
     for i in ls:
         if dpg.get_item_callback('bind_key_' + i) is None:
-            dpg.configure_item('bind_key_' + i, callback=vim_key_fe_callback)
+            dpg.configure_item('bind_key_' + i, callback=keyboard_event_handler)
         else:
             dpg.configure_item('bind_key_' + i, callback=None)
     dpg.set_value('shortcut', ' Shortcut Mode: '+('enabled' if dpg.get_item_callback('bind_key_h') else 'disabled'))
@@ -226,21 +236,51 @@ def toggle_bind_key():
 
 # mouse action and mask operation function
 def mouse_event_handler(sender, data):
-    type=dpg.get_item_info(sender)["type"]
-    print(type)
-    # if type=="mvAppItemType::mvMouseClickHandler":
-    #      dpg.set_value('mouse_clicked', f"Mouse id: {data} + Shift: {dpg.is_key_down(dpg.mvKey_Shift)}")
-    # if type=="mvAppItemType::mvMouseDoubleClickHandler":
-    #     dpg.set_value('mouse_doubleclicked', f"Mouse id: {data}")
-    if type=="mvAppItemType::mvMouseMoveHandler":
+    global mouse_point
+    type = dpg.get_item_info(sender)["type"]
+    if type == "mvAppItemType::mvMouseClickHandler":
+        mask_operation('add')
+    if type == "mvAppItemType::mvMouseDoubleClickHandler":
+        mask_operation('del')
+    if type == "mvAppItemType::mvMouseMoveHandler":
         dpg.set_value('mouse_move', f"Mouse pos: {data}")
+        mouse_point = data
+def mask_operation(mode):
+    if mode == 'add':
+        if boundary(mouse_point, SEG_FRAME_OFFSET_X, SEG_FRAME_OFFSET_Y, SEG_FRAME_END_X, SEG_FRAME_END_Y):
+            print('Clicked at', mouse_point)
+            i, j = restore(mouse_point, 'seg')
+            print('Frame pixel i:{} j:{}'.format(i, j))
+    elif mode == 'del':
+        if boundary(mouse_point, ORIGIN_FRAME_OFFSET_X, ORIGIN_FRAME_OFFSET_Y, ORIGIN_FRAME_END_X, ORIGIN_FRAME_END_Y):
+            print('DoubleClicked at', mouse_point)
+            i, j = restore(mouse_point, 'origin')
+            print('Frame pixel i:{} j:{}'.format(i, j))
+    else:   
+        pass
 
 
-# util functions
+# expression selection function
 def _selection(sender, app_data, user_data):
+    global selectable_item_exp
     for item in user_data:
         if item != sender:
             dpg.set_value(item, False)
+        else:
+            selectable_item_exp = dpg.get_item_configuration(sender)['label']
+            print('Now Selected exp are: ', selectable_item_exp)
+
+
+# util functions
+def boundary(p, ltx, lty, rbx, rby):
+    if (p[0] >= ltx and p[0] < rbx) and (p[1] >= lty and p[1] < rby):
+        return True
+    return False
+def restore(p, mode):
+    if mode == 'seg':
+        return [(p[0] - SEG_FRAME_OFFSET_X) // FRAME_FACTOR, (p[1] - SEG_FRAME_OFFSET_Y) // FRAME_FACTOR]
+    else: # origin
+        return [(p[0] - ORIGIN_FRAME_OFFSET_X) // FRAME_FACTOR, (p[1] - ORIGIN_FRAME_OFFSET_Y) // FRAME_FACTOR]
 
 
 # main UI definition function
@@ -261,32 +301,28 @@ def main(args):
             dpg.add_font_range_hint(dpg.mvFontRangeHint_Chinese_Full)
             
     # Shortcut mode
-    with dpg.handler_registry(show=False, tag="keyboard_handler"):
+    with dpg.handler_registry():
         dpg.add_key_press_handler(dpg.mvKey_LControl, callback=toggle_bind_key_callback)
-        dpg.add_key_press_handler(dpg.mvKey_H, callback=vim_key_fe_callback, tag='bind_key_h')
-        dpg.add_key_press_handler(dpg.mvKey_J, callback=vim_key_fe_callback, tag='bind_key_j')
-        dpg.add_key_press_handler(dpg.mvKey_K, callback=vim_key_fe_callback, tag='bind_key_k')
-        dpg.add_key_press_handler(dpg.mvKey_L, callback=vim_key_fe_callback, tag='bind_key_l')
-        dpg.add_key_press_handler(dpg.mvKey_W, callback=vim_key_fe_callback, tag='bind_key_w')
-        dpg.add_key_press_handler(dpg.mvKey_D, callback=vim_key_fe_callback, tag='bind_key_d')
-        dpg.add_key_press_handler(dpg.mvKey_A, callback=vim_key_fe_callback, tag='bind_key_a')
-        dpg.add_key_press_handler(dpg.mvKey_S, callback=vim_key_fe_callback, tag='bind_key_s')
-        dpg.add_key_press_handler(dpg.mvKey_Up, callback=vim_key_fe_callback, tag='bind_key_up')
-        dpg.add_key_press_handler(dpg.mvKey_Right, callback=vim_key_fe_callback, tag='bind_key_right')
-        dpg.add_key_press_handler(dpg.mvKey_Left, callback=vim_key_fe_callback, tag='bind_key_left')
-        dpg.add_key_press_handler(dpg.mvKey_Down, callback=vim_key_fe_callback, tag='bind_key_down')
+        dpg.add_key_press_handler(dpg.mvKey_H, callback=keyboard_event_handler, tag='bind_key_h')
+        dpg.add_key_press_handler(dpg.mvKey_J, callback=keyboard_event_handler, tag='bind_key_j')
+        dpg.add_key_press_handler(dpg.mvKey_K, callback=keyboard_event_handler, tag='bind_key_k')
+        dpg.add_key_press_handler(dpg.mvKey_L, callback=keyboard_event_handler, tag='bind_key_l')
+        dpg.add_key_press_handler(dpg.mvKey_W, callback=keyboard_event_handler, tag='bind_key_w')
+        dpg.add_key_press_handler(dpg.mvKey_D, callback=keyboard_event_handler, tag='bind_key_d')
+        dpg.add_key_press_handler(dpg.mvKey_A, callback=keyboard_event_handler, tag='bind_key_a')
+        dpg.add_key_press_handler(dpg.mvKey_S, callback=keyboard_event_handler, tag='bind_key_s')
+        dpg.add_key_press_handler(dpg.mvKey_Up, callback=keyboard_event_handler, tag='bind_key_up')
+        dpg.add_key_press_handler(dpg.mvKey_Right, callback=keyboard_event_handler, tag='bind_key_right')
+        dpg.add_key_press_handler(dpg.mvKey_Left, callback=keyboard_event_handler, tag='bind_key_left')
+        dpg.add_key_press_handler(dpg.mvKey_Down, callback=keyboard_event_handler, tag='bind_key_down')
+        dpg.add_mouse_click_handler(tag='mouse_clicked_handler', callback=mouse_event_handler)
+        dpg.add_mouse_double_click_handler(tag='mouse_doubelclicked_handler', callback=mouse_event_handler)
+        dpg.add_mouse_move_handler(tag='mouse_move_handler', callback=mouse_event_handler)
 
-    # mouse mode
-    with dpg.handler_registry(show=False, tag="mouse_handler"):
-        dpg.add_mouse_click_handler(tag='mouse_clicked_handler')
-        dpg.add_mouse_double_click_handler(tag='mouse_doubelclicked_handler')
-        dpg.add_mouse_move_handler(tag='mouse_move_handler')
-    for handler in dpg.get_item_children("mouse_handler", 1):
-        dpg.set_item_callback(handler, mouse_event_handler)
 
 
     # Main window for UI
-    with dpg.window(label="AirVLN Referring Expression 标注工具", width=WIDTH, height=HEIGHT, no_collapse=True, no_title_bar=True, no_move=True):
+    with dpg.window(label="AirVLN Referring Expression 标注工具", width=WINDOW_WIDTH, height=WINDOW_HEIGHT, no_collapse=True, no_title_bar=True, no_move=True):
 
         dpg.bind_font(font1)
 
@@ -314,33 +350,32 @@ def main(args):
             # split selector
             dpg.add_text("Toolbar - Split:", tag='split_hint')
             dpg.add_combo(['train', 'val_seen', 'val_unseen', 'test'], tag='split', \
-                default_value=default_split, width=90, callback=load_split_callback)
+                default_value=DEFAULT_SPLIT, width=90, callback=load_split_callback)
             # scene selector
             dpg.add_text('By Scene:', tag='scene_hint')
-            scene_list = ['None', 1, 2, 3, 4, 5, 8,10, 11, 12, 14, 16,\
-                17, 20, 22, 23, 25, 26]
-            dpg.add_combo(scene_list, tag='scene', default_value=default_scene, width=50) 
+            scene_list = ['None', 1, 2, 3, 4, 5, 8,10, 11, 12, 14, 16, 17, 20, 22, 23, 25, 26]
+            dpg.add_combo(scene_list, tag='scene', default_value=DEFAULT_SCENE, width=50) 
             load_json()
             # Load button
             dpg.add_button(callback=load_json_callback, label="Load", tag='load_btn')
             # episode_idx selector
             dpg.add_text('| State: Episode')
-            dpg.add_text(default_episode_idx, tag='episode_idx')
+            dpg.add_text(DEFAULT_EPISODE_IDX, tag='episode_idx')
             dpg.add_text('/{}'.format(len(episodes)), tag='len_episodes')
             dpg.add_button(arrow=True, direction=dpg.mvDir_Left, callback=idx_callback,\
                  user_data=['minus', 'episodes'])
-            dpg.add_drag_int(width=50, default_value=1, callback=idx_callback, min_value=1, max_value=len(episodes),\
+            dpg.add_drag_int(width=50, default_value=DEFAULT_EPISODE_IDX, callback=idx_callback, min_value=1, max_value=len(episodes),\
                  user_data=['drag', 'episodes'], tag='drag_int_episodes')
             dpg.add_button(arrow=True, direction=dpg.mvDir_Right, callback=idx_callback,\
                  user_data=['plus', 'episodes'])
             # frame_idx selector
             dpg.add_text('Frame')
-            dpg.add_text(default_frame_idx, tag='frame_idx')
-            dpg.add_text('/{}'.format(episodes[default_episode_idx-1]['len_frames']), tag='len_frames')
+            dpg.add_text(DEFAULT_FRAME_IDX, tag='frame_idx')
+            dpg.add_text('/{}'.format(episodes[DEFAULT_EPISODE_IDX-1]['len_frames']), tag='len_frames')
             dpg.add_button(arrow=True, direction=dpg.mvDir_Left, callback=idx_callback, \
                  user_data=['minus', 'frames'])
-            dpg.add_drag_int(width=90, default_value=1, callback=idx_callback, min_value=1,\
-                 max_value=episodes[default_episode_idx-1]['len_frames'],\
+            dpg.add_drag_int(width=90, default_value=DEFAULT_FRAME_IDX, callback=idx_callback, min_value=1,\
+                 max_value=episodes[DEFAULT_EPISODE_IDX-1]['len_frames'],\
                  user_data=['drag', 'frames'], tag='drag_int_frames')
             dpg.add_button(arrow=True, direction=dpg.mvDir_Right, callback=idx_callback, \
                  user_data=['plus', 'frames'])
@@ -350,11 +385,11 @@ def main(args):
         width, height, data, exp = load_imgs(False)
         width_seg, height_seg, data_seg, _ = load_imgs(True)
         # exps = []
-        if 'expressions' not in episodes[default_episode_idx-1]:
-            episodes[default_episode_idx-1]['expressions'] = []
+        if 'expressions' not in episodes[DEFAULT_EPISODE_IDX-1]:
+            episodes[DEFAULT_EPISODE_IDX-1]['expressions'] = []
         for value in exp['expressions'].values():
             # exps.append(value['exp']) 
-            episodes[default_episode_idx-1]['expressions'].append(value)
+            episodes[DEFAULT_EPISODE_IDX-1]['expressions'].append(value)
         # picture registry
         with dpg.texture_registry(show=False):
             dpg.add_dynamic_texture(width, height, data, tag="origin_frame")
@@ -366,8 +401,8 @@ def main(args):
         with dpg.group(horizontal=True):
             # space to show origin and segmentation frames
             with dpg.group():
-                dpg.add_image("origin_frame", width=width*FACTOR, height=height*FACTOR, pos=[0, 70])
-                dpg.add_image("seg_frame", width=width*FACTOR, height=height*FACTOR, pos=[0, 520])
+                dpg.add_image("origin_frame", width=width*FRAME_FACTOR, height=height*FRAME_FACTOR, pos=[ORIGIN_FRAME_OFFSET_X, ORIGIN_FRAME_OFFSET_Y])
+                dpg.add_image("seg_frame", width=width*FRAME_FACTOR, height=height*FRAME_FACTOR, pos=[SEG_FRAME_OFFSET_X, SEG_FRAME_OFFSET_Y])
             # space to show instructions, referring expressions highlighted navigation instructions,
             # tranlated navigation instructions and operating logs
             with dpg.group():
@@ -381,7 +416,7 @@ def main(args):
                 with dpg.popup('pop_up_edit_btn', modal=True, mousebutton=dpg.mvMouseButton_Left, tag="pop_edit_panel"):
                     with dpg.group(tag='pop_up_exps'):
                         with dpg.group(tag='pop_up_exps_sub'):
-                            for e in episodes[default_episode_idx-1]['expressions']:
+                            for e in episodes[DEFAULT_EPISODE_IDX-1]['expressions']:
                                 with dpg.group(horizontal=True, tag=e):
                                     dpg.add_input_text(default_value=e, tag=e+'input', user_data=[e, 'upd'], callback=exps_callback, on_enter=True)
                                     # dpg.add_button(label='Delete', user_data=e, callback=lambda s, a, u: dpg.delete_item(u))
@@ -393,7 +428,7 @@ def main(args):
                 items = []
                 with dpg.group(tag='exps'):
                     with dpg.group(tag='exps_sub'):
-                        for e in episodes[default_episode_idx-1]['expressions']:
+                        for e in episodes[DEFAULT_EPISODE_IDX-1]['expressions']:
                             items.append(dpg.add_selectable(label=e))
                     for item in items:
                         dpg.configure_item(item, callback=_selection, user_data=items)
@@ -402,16 +437,16 @@ def main(args):
 
 
         # Statusbar for loaded json file
-        with dpg.group(horizontal=True, pos=[5, 965]):
+        with dpg.group(horizontal=True, pos=[STATUS_BAR_OFFSET_X, STATUS_BAR_OFFSET_Y]):
             dpg.add_text('Statusbar -')
             dpg.add_text('Split: '+dpg.get_value('split'), tag='split_txt')
             dpg.add_text('Scene_id: '+dpg.get_value('scene'), tag='scene_txt')
-            dpg.add_text('Episode_id: '+episodes[default_episode_idx-1]['episode_id'], tag='episode_id')
-            dpg.add_text('Trajectory_id: '+episodes[default_episode_idx-1]['trajectory_id'], tag='trajectory_id')
+            dpg.add_text('Episode_id: '+episodes[DEFAULT_EPISODE_IDX-1]['episode_id'], tag='episode_id')
+            dpg.add_text('Trajectory_id: '+episodes[DEFAULT_EPISODE_IDX-1]['trajectory_id'], tag='trajectory_id')
             dpg.add_text('Mouse pos: []', tag='mouse_move')
             dpg.add_text('Shortcut Mode: '+('enabled' if dpg.get_item_callback('bind_key_h') else 'disabled'), tag='shortcut')
 
-    dpg.create_viewport(title='AirVLN Referring Expression Annotation Tool', width=WIDTH, height=HEIGHT)
+    dpg.create_viewport(title='AirVLN Referring Expression Annotation Tool', width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
     dpg.setup_dearpygui()
     dpg.show_viewport()
     dpg.start_dearpygui()
